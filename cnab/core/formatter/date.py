68c0dd65da4c016435da48bced60f90046c62d18
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-02-29 21:58:56 -0300

    fix: fix date generation with length 6

--- cnab/core/formatter/date.py
+++ cnab/core/formatter/date.py
@@ -1,6 +1,15 @@
 from datetime import datetime
+from typing import TYPE_CHECKING
 
-def format_date(value: datetime, field):
+if TYPE_CHECKING:
+    from cnab.core.field import CNABField
+
+def format_date(value: datetime, field: "CNABField"):
     if value == '0':
-        return value * 8
-    return value.strftime("%d%m%Y")
+        return value * field.length
+    if field.length == 8:
+        return value.strftime("%d%m%Y")
+    if field.length == 6:
+        return value.strftime("%d%m%y")
+    # TODO: raise custom exception here
+    raise Exception("invalid field")
