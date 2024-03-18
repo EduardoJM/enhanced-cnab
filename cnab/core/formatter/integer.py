1f1cba6fc973b4f5141e439cdec8b70958c6425a
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-03-18 12:57:22 -0300

    fix: limit length of integer field

--- cnab/core/formatter/integer.py
+++ cnab/core/formatter/integer.py
@@ -1,2 +1,5 @@
 def format_integer(value: int, field):
-    return str(value).rjust(field.length, '0')
+    value = str(value).rjust(field.length, '0')
+    if len(value) > field.length:
+        value = value[0:field.length]
+    return value
