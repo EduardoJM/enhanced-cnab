677b0f9622988a8d3adfff2a0ed49dfb4297b4eb
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-03-19 13:11:36 -0300

    refactor: remove remove_accents unused code

--- cnab/core/formatter/alfa.py
+++ cnab/core/formatter/alfa.py
@@ -1,29 +1,7 @@
-import re
 from unidecode import unidecode
-
-def remove_accents(text: str) -> str:
-    regexes = [
-        r'\xc3[\x80-\x85]',
-        r'\xc3\x87',
-        r'\xc3[\x88-\x8b]',
-        r'\xc3[\x8c-\x8f]',
-        r'\xc3([\x92-\x96]|\x98)',
-        r'\xc3[\x99-\x9c]',
-        r'\xc3[\xa0-\xa5]',
-        r'\xc3\xa7',
-        r'\xc3[\xa8-\xab]',
-        r'\xc3[\xac-\xaf]',
-        r'\xc3([\xb2-\xb6]|\xb8)',
-        r'\xc3[\xb9-\xbc]'
-    ]
-    replacers = ['A', 'C', 'E', 'I', 'O', 'U', 'a', 'c', 'e', 'i', 'o', 'u']
-    for regex, repl in zip(regexes, replacers):
-        text = re.sub(regex, repl, text).lower()
-    return text
 
 def prepare_text(text: str) -> str:
     return unidecode(text.strip()).upper()
-    return remove_accents(text.strip()).upper()
 
 def format_alfa(value, field):
     value = prepare_text(str(value))
